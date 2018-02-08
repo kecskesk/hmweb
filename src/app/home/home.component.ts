656fbf7309e15c6bf4046be3dfbf399d98f0372d
Kecskés Krisztián <kecskesk@sonrisa.hu>
2018-02-08 18:45:08 +0100

    put imageurl to storage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage } from 'angularfire2/storage';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
+	imageUrl: string;
 
-  constructor() { }
+  constructor(private storage: AngularFireStorage,
+			  private db: AngularFireDatabase) {
+  	this.db.object('home-picture').valueChanges().subscribe((url) => {
+		const ref = this.storage.ref('home-picture/'+url);
+		ref.getDownloadURL().subscribe((imageUrl) => {
+			this.imageUrl = imageUrl;
+		});
+	});
+  }
 
   ngOnInit() {
   }
