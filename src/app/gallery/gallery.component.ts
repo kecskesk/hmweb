f379e7d34daf0b7dcc9ae706f8a86ec51ecc9f7b
Kecskés Krisztián <k3csk3s@gmail.com>
2018-03-17 17:48:51 +0100

    gallery viewer

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,63 +1,82 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation, NgxGalleryImageSize } from 'ngx-gallery';
+import { AngularFireDatabase, SnapshotAction } from 'angularfire2/database';
+import { AngularFireStorage } from 'angularfire2/storage';
 
 @Component({
-  selector: 'app-gallery',
-  templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.less']
+    selector: 'app-gallery',
+    templateUrl: './gallery.component.html',
+    styleUrls: ['./gallery.component.less']
 })
 export class GalleryComponent implements OnInit {
+    imageAlbums: Array<ImageAlbum> = [];
+    imageAlbumKeys: Array<SnapshotAction> = [];
+    openAlbum: ImageAlbum;
+    openAlbumKey: string;
 
-  constructor() { }
-  galleryOptions: NgxGalleryOptions[];
-  galleryImages: NgxGalleryImage[];
+    constructor(private storage: AngularFireStorage, private db: AngularFireDatabase) {
+        this.db.list('gallery').valueChanges().subscribe((res) => {
+            this.imageAlbums = res as Array<ImageAlbum>;
+        });
+        this.db.list('gallery').snapshotChanges().subscribe((res) => {
+            this.imageAlbumKeys = res as Array<SnapshotAction>;
+        });
+    }
+    galleryOptions: NgxGalleryOptions[];
+    galleryImages: NgxGalleryImage[];
 
-  ngOnInit(): void {
+    ngOnInit(): void {
 
-      this.galleryOptions = [
-          {
-              width: '600px',
-              height: '400px',
-              thumbnailsColumns: 4,
-              imageAnimation: NgxGalleryAnimation.Slide,
-              imageSize: NgxGalleryImageSize.Contain
-          },
-          // max-width 800
-          {
-              breakpoint: 800,
-              width: '100%',
-              height: '600px',
-              imagePercent: 80,
-              thumbnailsPercent: 20,
-              thumbnailsMargin: 20,
-              thumbnailMargin: 20,
-              imageSize: NgxGalleryImageSize.Contain
-          },
-          // max-width 400
-          {
-              breakpoint: 400,
-              preview: false,
-              imageSize: NgxGalleryImageSize.Contain
-          }
-      ];
+        this.galleryOptions = [
+            {
+                width: '600px',
+                height: '400px',
+                thumbnailsColumns: 4,
+                imageAnimation: NgxGalleryAnimation.Slide,
+                imageSize: NgxGalleryImageSize.Contain
+            },
+            // max-width 800
+            {
+                breakpoint: 800,
+                width: '100%',
+                height: '600px',
+                imagePercent: 80,
+                thumbnailsPercent: 20,
+                thumbnailsMargin: 20,
+                thumbnailMargin: 20,
+                imageSize: NgxGalleryImageSize.Contain
+            },
+            // max-width 400
+            {
+                breakpoint: 400,
+                preview: false,
+                imageSize: NgxGalleryImageSize.Contain
+            }
+        ];
 
-      this.galleryImages = [
-          {
-              small: 'assets/1-small.jpg',
-              medium: 'assets/1-medium.jpg',
-              big: 'assets/1-big.jpg'
-          },
-          {
-              small: 'assets/2-small.jpg',
-              medium: 'assets/2-medium.jpg',
-              big: 'assets/2-big.jpg'
-          },
-          {
-              small: 'assets/3-small.jpg',
-              medium: 'assets/3-medium.jpg',
-              big: 'assets/3-big.jpg'
-          }
-      ];
-  }
+        this.galleryImages = [];
+    }
 
+    selectAlbum(i: number) {
+        this.openAlbumKey = this.imageAlbumKeys[i].key;
+        this.db.list('gallery/' + this.openAlbumKey + '/images').valueChanges().subscribe((imagesResult) => {
+            let imagesArray = imagesResult as Array<string>;
+            imagesArray.forEach((url) => {
+                this.galleryImages = [];
+                this.storage.ref('gallery/' + url).getDownloadURL().subscribe((fullUrl) => {
+                    this.galleryImages.push({
+                        small: fullUrl, 
+                        medium: fullUrl,
+                        big: fullUrl
+                    });
+                    this.openAlbum = this.imageAlbums[i];
+                });
+            });
+        });
+    }
+}
+
+export class ImageAlbum {
+    title: string;
+    imageurls: any;
 }
