e160a1713346034ad8f94572896ef33bf45c033a
Kecskes Krisztian <kecskesk@sonrisa.hu>
2018-07-29 23:24:42 +0200

    unify colors, fix blog design, thumbnail, tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,27 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {Router} from '@angular/router';
+import createSpyObj = jasmine.createSpyObj;
+import {RouterTestingModule} from '@angular/router/testing';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
+      ],
+      imports: [
+        RouterTestingModule
+      ],
+      providers: [
+        NavbarComponent,
+        { provide: Router, useValue: createSpyObj<Router>('Router', ['navigate'])  }
+      ],
+      schemas: [
+        NO_ERRORS_SCHEMA
       ],
     }).compileComponents();
   }));
@@ -15,18 +30,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
